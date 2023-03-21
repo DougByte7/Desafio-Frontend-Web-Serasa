f91ef36fa68ae34882e087dfe2cf052d90544c7c
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-03-21 20:48:55 -0300

    Add Text component

---
+++ desafio-frontend-serasa/src/components/Text/Text.tsx
@@ -0,0 +1,42 @@
+import { forwardRef, ReactNode } from "react"
+import styles from "@/components/Text/Text.module.css"
+
+export type TextSize =
+  | "display"
+  | "lg"
+  | "md"
+  | "sm"
+  | "xs"
+  | "subheading"
+  | "body"
+
+export interface TextProps {
+  component?: keyof JSX.IntrinsicElements
+  size?: TextSize
+  span?: boolean
+  bold?: boolean
+  children?: ReactNode
+}
+
+export const Text = forwardRef<HTMLHeadingElement, TextProps>(
+  function TextComponent(
+    { children, component, size, span, bold, ...rest },
+    ref
+  ) {
+    const Tag = (component ?? (span ? "span" : "div")) as React.ElementType
+
+    return (
+      <Tag
+        className={`
+            ${styles.text} 
+            ${styles[`text--${size ?? "body"}`]}
+            ${bold ? styles["text--bold"] : ""}
+        `}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Tag>
+    )
+  }
+)
