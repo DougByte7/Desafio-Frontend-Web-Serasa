f4e6ad449486bca8c6b0e115a54452fda43ddff8
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-03-21 22:20:37 -0300

    Add Text component specs

---
+++ desafio-frontend-serasa/src/components/Text/README.md
@@ -0,0 +1,52 @@
+# Text
+
+Utilize este componente para exir elementos textuais.
+
+```jsx
+<Text>Texto normal</Text>
+```
+
+## Tamanhos
+
+Utilize o prop `size` do tipo `TextSize` descrito na tabela abaixo para alterar o tamanho do texto
+
+| Valor      | Descrição (desktop / mobile)          |
+| ---------- | ------------------------------------- |
+| display    | Tamanho destaque: 3rem / 2.5rem       |
+| lg         | Tamanho grande: 2.5rem / 2rem         |
+| md         | Tamanho médio: 2rem / 1.5rem          |
+| sm         | Tamanho pequeno: 1.5rem / 1.25rem     |
+| xs         | Tamanho minusculo: 1.25rem / 1.125rem |
+| subheading | Tamanho subtítulo: 1rem               |
+| body       | (default) Tamanho padrão: 1rem        |
+
+```jsx
+<Text size="lg">Texto grande</Text>
+```
+
+## Prop bold
+
+Utilize o prop `bold` para deixa o texto em negrito
+
+```jsx
+<Text bold>Texto grande</Text>
+```
+
+## Polimorfia
+
+O Text é um [componente polimórfico](https://dev.to/julioxavierr/make-your-react-components-more-flexible-using-polymorphism-5fhi), isto é, você pode alterar o componente raiz por qualquer outra tag HTML.
+
+Para isto utilize o prop `component`, seu valor padrão é `div`
+
+```jsx
+<Text component="p">Elemento de parágrafo</Text>
+```
+
+# Prop span
+
+Utilize o prop `span` como uma abreviação para `component="span"`
+
+```jsx
+<Text span>O mesmo que abaixo</Text>
+<Text component="span">O mesmo que acima</Text>
+```
---
+++ desafio-frontend-serasa/src/components/Text/Text.spec.tsx
@@ -0,0 +1,85 @@
+import { render } from "@testing-library/react"
+import { Text, TextProps, TextSize } from "./Text"
+
+const renderText = (props: TextProps) => render(<Text {...props} />)
+
+const defaultProps: TextProps = {
+  children: "test",
+}
+
+describe("Text", () => {
+  it("display the its children", () => {
+    const { getByText } = renderText({ ...defaultProps })
+    getByText("test")
+  })
+
+  it("renders a simples div with text and body styles", () => {
+    const {
+      container: { firstChild },
+    } = renderText(defaultProps)
+
+    expect((firstChild as HTMLElement).tagName).toBe("DIV")
+    expect((firstChild as HTMLElement).classList.contains(`text`)).toBe(true)
+    expect((firstChild as HTMLElement).classList.contains(`text--body`)).toBe(
+      true
+    )
+    expect((firstChild as HTMLElement).classList.contains(`text--bold`)).toBe(
+      false
+    )
+  })
+
+  it("renders a custom component based on component prop", () => {
+    const {
+      container: { firstChild },
+    } = renderText({ ...defaultProps, component: "p" })
+    expect((firstChild as HTMLElement).tagName).toBe("P")
+  })
+
+  it("renders as span when span prop is set", () => {
+    const {
+      container: { firstChild },
+    } = renderText({ ...defaultProps, span: true })
+    expect((firstChild as HTMLElement).tagName).toBe("SPAN")
+  })
+
+  it("change styles based on size prop", () => {
+    const classList: Set<TextSize> = new Set([
+      "display",
+      "lg",
+      "md",
+      "sm",
+      "xs",
+      "subheading",
+      "body",
+    ] as TextSize[])
+
+    classList.forEach((className) => {
+      const {
+        container: { firstChild },
+      } = renderText({ ...defaultProps, size: className })
+
+      const subSet = new Set(classList)
+      subSet.delete(className)
+
+      expect(
+        Array.from(subSet).some((className) =>
+          (firstChild as HTMLElement).classList.contains(`text--${className}`)
+        )
+      ).toBe(false)
+
+      expect(
+        (firstChild as HTMLElement).classList.contains(`text--${className}`)
+      ).toBe(true)
+    })
+  })
+
+  it("sets 'font-weight: bold' based on bold prop", () => {
+    const {
+      container: { firstChild },
+    } = renderText({ ...defaultProps, bold: true })
+
+    expect((firstChild as HTMLElement).classList.contains(`text--bold`)).toBe(
+      true
+    )
+  })
+})
