b95e445e7f4820c792b0ba048fadd9ec4053adc2
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2023-03-23 18:34:56 -0300

    Add textinput component

---
+++ desafio-frontend-serasa/src/components/Inputs/TextInput/TextInput.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react"
+import Text from "@/components/Typography/Text"
+import styles from "./TextInput.module.css"
+
+export type TextInputProps = {
+  label: string
+} & JSX.IntrinsicElements["input"]
+
+export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  function TextInputComponent({ label, ...rest }, ref) {
+    return (
+      <Text bold>
+        <label className={styles["text-input"]}>
+          {label}
+          <input
+            className={styles["text-input__input"]}
+            ref={ref}
+            type="text"
+            {...rest}
+          />
+        </label>
+      </Text>
+    )
+  }
+)
